7ad474ac8488812642d7e23b0cb29f2b48caac69
Krishna punj singh <krishnapunjsingh@gmail.com>
2025-07-16 19:39:36 +0530

    server.js updated for vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   return (
 
 
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+    <div className="bg-[url('/bgImage.svg')] bg-contain">
       <Toaster />
       <Routes>
         
--- server/server.js
+++ server/server.js
@@ -55,6 +55,10 @@ app.use("/api/messages", messageRouter)
 //connect to mongodb
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, ()=> console.log("Server is running on port " + PORT));
+if(process.env.NODE_ENV !== "production"){
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, ()=> console.log("Server is running on port " + PORT));
+}
 
+//Export server for Vercel
+export default server;
